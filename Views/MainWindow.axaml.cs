4db95b2579e1045ccd091923425bfba0b2b01423
Meursau1T <meursaultw@outlook.com>
2025-08-08 10:38:44 +0800

    图标

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,9 +1,18 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Input;
 
 namespace GoldMonitor.Views;
 
 public partial class MainWindow : Window {
     public MainWindow() {
         InitializeComponent();
+    }
+    private void TitleBar_PointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (e.GetCurrentPoint(null).Properties.IsLeftButtonPressed)
+        {
+            BeginMoveDrag(e);
+        }
     }
 }
